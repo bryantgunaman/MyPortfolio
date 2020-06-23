997f1dec64370b1c6fad9dcf82ac1419a06e3996
bryantgunaman <bryantgunaman@gmail.com>
2020-06-23 12:29:59 -0700

    added layout template

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+  </div>
   );
 }
 
