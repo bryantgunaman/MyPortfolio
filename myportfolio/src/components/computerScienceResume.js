f8cd67627372365326d393277f6f47211cf447e0
bryantgunaman <bryantgunaman@gmail.com>
2020-06-23 22:08:08 -0700

    added pdfViewer

---
+++ myportfolio/src/components/computerScienceResume.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Viewer, { Worker } from '@phuocng/react-pdf-viewer';
+import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
+
+class ComputerScienceResume extends Component {
+    render () {
+        return (
+            <div>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
+                <div style={{ height: '750px' }}>
+                    <Viewer fileUrl={process.env.PUBLIC_URL + "/Resumes/Bryant Gunaman's Computer Science Resume.pdf"} />
+                </div>
+                </Worker>
+            </div>
+        )
+    }
+}
+
+export default ComputerScienceResume;
+
