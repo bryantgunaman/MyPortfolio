0976bd633d0be81380eab99987be3d5da4bdf7da
bryantgunaman <bryantgunaman@gmail.com>
2020-06-26 19:54:01 -0700

    changed build path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use('/static', express.static(path_join(__dirname, '../client/build')));
+app.use('/static', express.static(path_join(__dirname, '/build')));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
