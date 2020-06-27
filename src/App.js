fa4efb0a29580dcdb61b2cf561e4cb328c6e332a
bryantgunaman <bryantgunaman@gmail.com>
2020-06-26 20:04:36 -0700

    disabled server for now

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import { Link } from 'react-router-dom';
 
 class App extends Component {
 
-  state = {
-    data: null
-  };
+  // state = {
+  //   data: null
+  // };
 
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  // componentDidMount() {
+  //     // Call our fetch function below once the component mounts
+  //   this.callBackendAPI()
+  //     .then(res => this.setState({ data: res.express }))
+  //     .catch(err => console.log(err));
+  // }
+  //   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  // callBackendAPI = async () => {
+  //   const response = await fetch('/express_backend');
+  //   const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
+  //   if (response.status !== 200) {
+  //     throw Error(body.message) 
+  //   }
+  //   return body;
+  // };
 
 
   render() {
