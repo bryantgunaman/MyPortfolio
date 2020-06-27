e8b9fa2ea1b9e7f6c6521a522a79ef93abb08a04
bryantgunaman <bryantgunaman@gmail.com>
2020-06-26 19:48:30 -0700

    added app use

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use('/static', express.static(path_join(__dirname, '../client/build')));
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
