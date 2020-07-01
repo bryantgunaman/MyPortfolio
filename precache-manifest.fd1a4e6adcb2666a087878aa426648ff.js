37388a42b7313029f26223274529116aaa6ff21d
bryantgunaman <bryantgunaman@gmail.com>
2020-07-01 16:28:26 -0700

    Updates

--- precache-manifest.fd1a4e6adcb2666a087878aa426648ff.js
+++ precache-manifest.fd1a4e6adcb2666a087878aa426648ff.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69fb77d0aaa12d063292542b6eb09cf7",
+    "revision": "137033baa42cf0b168c0b13271536a4a",
     "url": "/MyPortfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MyPortfolio/static/css/2.7bc30736.chunk.css"
   },
   {
-    "revision": "273c7cc1035d17590a04",
+    "revision": "009df55a9c0835fc3be1",
     "url": "/MyPortfolio/static/css/main.38722b4c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MyPortfolio/static/js/2.b89a930c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "273c7cc1035d17590a04",
-    "url": "/MyPortfolio/static/js/main.804d6ba8.chunk.js"
+    "revision": "009df55a9c0835fc3be1",
+    "url": "/MyPortfolio/static/js/main.bb01fe3d.chunk.js"
   },
   {
     "revision": "93c65b4db3af1fa805d2",
